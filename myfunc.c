00460f28cdd08f987be0de467801f1c11fbf1218
edfink234 <edward.finkelstein@cern.ch>
2025-04-13 14:04:21 -0700

    Adding option to update plots every n seconds

---
+++ myfunc.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+// This is your performance-critical function that you wish to call from JS.
+// For example, a function that computes f(x) = sin(x) * cos(x).
+double f(double x) {
+    return sin(x) * cos(x);
+}
+
