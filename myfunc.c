9a323ff73dea6f715f8a200535473a89463b2a04
edfink234 <edward.finkelstein@cern.ch>
2025-04-17 01:05:30 -0700

    Adding eigen decomp to ParticleVsSolitonDynamics.html

--- myfunc.c
+++ myfunc.c
@@ -2,7 +2,8 @@
 
 // This is your performance-critical function that you wish to call from JS.
 // For example, a function that computes f(x) = sin(x) * cos(x).
-double f(double x) {
-    return sin(x) * cos(x);
-}
 
+double sech(double x) {
+    return 1.0/cosh(x);
+}
+   
